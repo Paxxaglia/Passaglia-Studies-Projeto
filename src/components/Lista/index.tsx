8b17b55a051bbbbdf07d96a0653ebfce12ba7da9
Iago Passaglia <iago210669@gmail.com>
2023-05-25 19:20:24 -0300

    Criando componente da lista e Textos

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { deflate } from "zlib";
+
+function Lista() {
+    return (
+        <aside>
+            <h2>Estudos do Dia</h2>
+            <ul>
+                <li>
+                    <h3> React</h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3> JavaScript</h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
